d868faa83650e6535d64c53632624b95fd86716f
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2021-06-07 09:16:32 -0400

    Create day1.swift

---
+++ day1.swift
@@ -0,0 +1,26 @@
+//1. variables
+var operatingSystem = "macOS"
+
+//2. Strings and ints
+var dogBreed = "samoyed"
+
+//3. Multi line strings
+var burns = """
+The best laid schemes
+o' mice an' men
+gang aft agley
+"""
+
+//4. Booleans
+var animated = false
+
+//5. String inerplotion
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
+
+//6. Constants
+let taylor = "swift"
+
+//7. Type annotations
+var sales: Double = 100_000
+
