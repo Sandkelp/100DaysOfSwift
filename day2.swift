3528cd118b8ef841b54312511443569e9a92f380
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2021-06-07 12:20:34 -0400

    Create day2.swift

---
+++ day2.swift
@@ -0,0 +1,15 @@
+//1. Arrays
+var averages = [98.5, 97.1, 99.9]
+
+//2. Sets
+//random assortment so faster retrival of data
+var readings = Set([true, false, true, true])
+
+//3. Tuples
+var website = (name: "Apple", url: "www.apple.com")
+
+//4. Dictionaries
+//you can use strings to get the data instead of an index
+var roles = ["captain": "Mal", "engineer": "Kaylee"]
+
+//5. 
