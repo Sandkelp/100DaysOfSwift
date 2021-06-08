b17ed7409dcda1efe1b5bd098216ea98a2677cf8
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2021-06-08 10:14:24 -0400

    Update day2.swift

--- day2.swift
+++ day2.swift
@@ -7,6 +7,7 @@ var readings = Set([true, false, true, true])
 
 //3. Tuples
 var website = (name: "Apple", url: "www.apple.com")
+var fibonacci = (1, 1, 2, 3, 5, 8)
 
 //4. Dictionaries
 //you can use strings to get the data instead of an index
@@ -17,4 +18,18 @@ var favoriteColors = Dictionary<String, String>()
 
 //6. Enumerations
 //fixed set of data like days in a week not places visited
+enum Weather {
+    case sunny
+    case windy(speed: Int)
+    case rainy(chance: Int, amount: Int)
+}
+
+//7. Enums with datatypes
+enum Mood: Int {
+    case happy
+    case sad
+    case grumpy
+    case sleepy
+    case hungry
+}
 
